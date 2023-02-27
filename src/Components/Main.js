c9e723f9969d8f334e42d879d00d4e32ccd064f9
Nikita <nameday31@gmail.com>
2023-02-26 22:28:51 -0500

    init

---
+++ src/Components/Main.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+export default function Main() {
+    return (
+        <div>
+            <button>
+                <Link to="/white">
+                    Click
+                </Link>
+            </button>
+        </div>
+    )
+}
